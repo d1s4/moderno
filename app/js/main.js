cd89be8344b34e5a222556ee2fdd3d0e3ec3a6f9
Roman <56957871+d1s4@users.noreply.github.com>
2019-11-30 20:00:15 +0200

    added size filter

--- app/js/main.js
+++ app/js/main.js
@@ -7,7 +7,7 @@ $(function () {
         readOnly: true,
     });
 
-    $('.product-slider__inner').slick({
+    $(".product-slider__inner").slick({
         slidesToShow: 4,
         slidesToScroll: 4,
         dots: true,
@@ -24,6 +24,17 @@ $(function () {
         prefix: "$"
     });
 
+    $(".icon-th-list").on('click', function () {
+        $(".product-page__items > .product__item").addClass("list"),
+            $(".icon-th-large").removeClass("active"),
+            $(".icon-th-list").addClass("active")
+    });
+
+    $(".icon-th-large").on('click', function () {
+        $(".product-page__items > .product__item").removeClass("list"),
+            $(".icon-th-list").removeClass("active"),
+            $(".icon-th-large").addClass("active")
+    });
     var mixer = mixitup('.products__inner-box');
 
 

8b26dc4973493a09e2779b02509c58fd5a769830
Roman <56957871+d1s4@users.noreply.github.com>
2019-12-12 08:29:07 +0200

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -11,7 +11,31 @@ $(function () {
         slidesToShow: 4,
         slidesToScroll: 4,
         dots: true,
-        arrows: false
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 1900,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 1441,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 801,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            },
+        ]
     });
 
     $(".js-range-slider").ionRangeSlider({
